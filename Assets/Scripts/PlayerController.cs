ad17ce408b395d05a2177ac0eef039cbde35a8b8
Jeremiah Sanchez <j7sanchez13@gmail.com>
2020-07-31 17:55:17 -0400

    First Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Animator animator;
+
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        animator.SetFloat("Movement", moveHorizontal);
+        
+        Vector2 movement = new Vector2 (moveHorizontal, 0);
+        rb2d.AddForce(movement * speed);
+    }
+}
