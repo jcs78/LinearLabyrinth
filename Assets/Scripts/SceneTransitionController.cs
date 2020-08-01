75a0b36d7909f739667cc96bfa565d101d8ac280
Jeremiah Sanchez <j7sanchez13@gmail.com>
2020-08-01 15:57:27 -0400

    Completed TitleScreen scene and added transition to MainMenu scene.

---
+++ Assets/Scripts/SceneTransitionController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionController : MonoBehaviour
+{
+    public string levelName;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("z"))
+        {
+            SceneManager.LoadScene(levelName);
+            Debug.Log("Everything's Peachy");
+        }
+    }
+}
