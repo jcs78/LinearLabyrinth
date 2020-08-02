5aea27891ae7493ab51cb393332fc1b6f245dbc5
Jeremiah Sanchez <j7sanchez13@gmail.com>
2020-08-01 21:22:15 -0400

    Removed HighlightOpt.cs and added HighlightChoiceController (which fully functions).

---
+++ Assets/Scripts/ActivatePressZController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatePressZController : MonoBehaviour
+{
+    public GameObject pressZ;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        pressZ.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pressZ.SetActive(true);
+    }
+}
