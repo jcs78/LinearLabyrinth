4814e1f3f5ff0327e0cce630ca7d17848214892e
Jeremiah Sanchez <j7sanchez13@gmail.com>
2020-08-02 18:11:38 -0400

    All Main Enter/Exit Doors are working functionally. (Also, changed a bit of stuff in the Hub scene.)

---
+++ Assets/Scripts/DoorTransitionController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTransitionController : MonoBehaviour
+{
+    public float x_position;
+    public float y_position;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && Input.GetKey("z"))
+        {
+            other.gameObject.transform.position = new Vector2(x_position, (y_position - 0.25f));
+        }
+    }
+}
