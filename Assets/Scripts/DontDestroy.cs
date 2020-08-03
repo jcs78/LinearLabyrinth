06a425223b2b7e7509aa398aaafae2804a96d92d
Jeremiah Sanchez <j7sanchez13@gmail.com>
2020-08-03 09:39:33 -0400

    Attempted to use DontDestroy.cs, but failed.

---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject[] objsPlayer = GameObject.FindGameObjectsWithTag("Player");
+        GameObject[] objsRedSKF = GameObject.FindGameObjectsWithTag("SKF (Red)");
+        GameObject[] objsBlueSKF = GameObject.FindGameObjectsWithTag("SKF (Blue)");
+
+        if (objsPlayer.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        if (objsRedSKF.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        if (objsBlueSKF.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
