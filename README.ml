88dd6b7ba80f568ec7ea5d283bbf35d4c73aab39
Jeremiah Sanchez <j7sanchez13@gmail.com>
2020-08-03 16:46:39 -0400

    Added a README (along with a few minor adjustments).

---
+++ README.ml
@@ -0,0 +1,14 @@
+"Hello, this is my game, Linear Lanbyrinth. This is a 2D side-scrolling, Metroidvania that takes place deep within a Labyrinth. Your goal is to find all the star fragments of a variety of colors and pair them together to create a key to unlock the next area. Are you reach to see what's in store in Linear Labyrinth?"
+
+"Here are the controls to play the game:"
+"(Title Screen / Main Menu)"
+">> Z | Select"
+">> Esc | Close Application"
+"Left and Right Arrow | Highlight Option to the left or right."
+"(Hub / Areas)"
+"z | Enter and Exit Doorways"
+"x | Use Ability of Character Playeing As"
+"Left and Right Arrow Keys | Move Left or Right"
+
+"Note: The Cave Explorer's non-implemented ability can be escaped using the Esc button."
+
